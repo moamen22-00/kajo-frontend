e36d342fe2504fbf9f4b66a3b0cdbbc100c883f7
Ahmed Momen <momenjamal2002@gmail.com>
2026-03-17 10:02:27 +0300

    fix doctor dashboard import path

--- src/Layout/AppLayout/AppLayout.jsx
+++ src/Layout/AppLayout/AppLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import MainNavbar from '../../components/Navbar/mainNavbar'
+import MainNavbar from '../../components/Navbar/MainNavbar'
 
 export default function AppLayout() {
   return ( <>
